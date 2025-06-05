061e0c33097d1837c83cf31e457f9134f7dd7c69
kierankaelin <kaelin@kieran.at>
2025-06-05 21:23:13 +0200

    fix: more width for two digit numbers

--- src/sheets/const.ts
+++ src/sheets/const.ts
@@ -6,10 +6,10 @@ export const SHEETS = {
 
 export const SHEET_AREAS = {
   obstacleAll: `${SHEETS.obstacle}!A2`,
-  obstacleStartNumbers: `${SHEETS.obstacle}!B2:B1000`,
+  obstacleStartNumbers: `${SHEETS.obstacle}!A2:A1000`,
 
   relayAll: `${SHEETS.relay}!A2`,
-  relayStartNumbers: `${SHEETS.relay}!B2:B1000`,
+  relayStartNumbers: `${SHEETS.relay}!A2:A1000`,
 
   teams: `${SHEETS.teams}!A2:C1000`,
 };
