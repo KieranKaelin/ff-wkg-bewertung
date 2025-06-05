91fde4b6b8d43b0684c4b54e8f8a63d91d68eaba
kierankaelin <kaelin@kieran.at>
2025-06-05 18:25:20 +0200

    feat: buffer polyfill 3

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,18 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   base: "/ff-wkg-bewertung/",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), nodePolyfills()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./vitest.setup.mjs",
-  },
-  resolve: {
-    alias: {
-      buffer: "buffer/",
-    },
   },
 });
